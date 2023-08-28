511a683ab1d3cd16b3eafaae6195a130b42587ff
Chandira77 <irachand620@gmail.com>
2023-08-28 16:56:23 +0545

    First commit

---
+++ basic_python/firstprogram.py
@@ -0,0 +1,3 @@
+print("Hello world") 
+
+print("ValueOne", "ValueTwo", "ValueThree", "ValueFour", sep=' | ')
---
+++ basic_python/userinput.py
@@ -0,0 +1,7 @@
+name= input("What is your name")   #input function is used to take user defined values
+print("hello" + name)
+
+firstnum = input("Enter a number")
+Secondnum = input("Enter another number") 
+sum = firstnum + Secondnum   # '+' symbol is used to concat two variables
+print("The sum =" ,sum)

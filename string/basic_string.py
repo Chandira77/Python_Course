fafb51e2de2e405b855b0a00db01fbf544cdfaae
Chandira77 <irachand620@gmail.com>
2024-03-04 16:18:13 +0545

    All about string

---
+++ string/basic_string.py
@@ -0,0 +1,34 @@
+a = 'hello'     #string to a variable
+print(a)   
+
+#multiline string
+b = """hey i'm Ira Chand. now i'm practicing python programming language. while practing i got a lots of confusions, errors which i didn't understand"""
+print(b)
+
+#strings are array
+c = "Sanothimi, Bhaktapur"
+print(c[1])
+
+#looping through a string
+for d in 'banana':
+  print(d)
+
+
+# string length
+e = "hello, world"
+print(len(e))
+
+
+#check string
+f = "i love myself"
+print("love" in f)
+print("what" in f)
+
+# if statement
+g = "i love myself "
+if "love" in g:
+  print("yes, it's present")
+
+  # if not statement
+  h = "hey, what's up?"
+print("Amazing" not in h)

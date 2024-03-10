852e48f1a222fe899f53e1871262266c2649bc7f
Chandira77 <irachand620@gmail.com>
2024-03-10 13:43:54 +0545

    about operators

---
+++ operators/logical_operators.py
@@ -0,0 +1,17 @@
+x = 4
+print(x>3 and x<7)
+print(True and True)
+print(False and False)
+print(False and True)
+print(True and False)
+
+#or operator
+print(True or True)   #True
+print(False or False) #False
+print(False or True) #True
+print(True or False) #True
+
+#not operator
+a = False
+b = not(a)
+print(b)
